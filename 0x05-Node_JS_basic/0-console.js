d02c15b8726f6439fdde047acb9c5861d9e425f6
Beatrice A Winful <beatriceawinful197@gmail.com>
2023-11-23 05:34:13 +0000

    nodejs

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author James Aidoo <https://github.com/ghjimchris>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
